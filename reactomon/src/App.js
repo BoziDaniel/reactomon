32b394b3e2177c53c1bdbe805e26bb202c1a2807
BoziDaniel <daniel.bozi88@gmail.com>
2020-04-04 20:39:10 +0200

    replaced componentDidMount in App.js

--- reactomon/src/App.js
+++ reactomon/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import NavBar from "./components/layout/NavBar";
 import { BrowserRouter as Router, Route } from "react-router-dom";
@@ -7,62 +7,61 @@ import axios from "axios";
 import Types from "./components/pages/Types";
 import PokemonDetail from "./components/pages/PokemonDetail";
 
-class App extends Component {
-  state = {
-    pokemons: [],
-    types: [],
-    pokemonData: []
-  };
+const App = () => {
+  const [pokemons, setPokemons] = useState([]);
+  const [types, setTypes] = useState([]);
 
-  getPokemonData = (url)=>axios.get(url).then(resp=>{return resp.data})
-  getId= url => url.substring(url.lastIndexOf('/') -1)[0];
-  
-  componentDidMount() {
-    axios.get("https://pokeapi.co/api/v2/pokemon").then(res => {
-      this.setState({ pokemons: res.data.results });
-      axios.get("https://pokeapi.co/api/v2/type").then(res => {
-        this.setState({ types: res.data.results });
+  const getPokemonData = (url) =>
+    axios.get(url).then((resp) => {
+      return resp.data;
+    });
+  const getId = (url) => url.substring(url.lastIndexOf("/") - 1)[0];
+  useEffect(() => {
+    axios.get("https://pokeapi.co/api/v2/pokemon").then((res) => {
+      setPokemons(res.data.results);
+      axios.get("https://pokeapi.co/api/v2/type").then((res) => {
+        setTypes(res.data.results);
       });
     });
-    const pokemons = this.state.pokemons;
-    pokemons.map(pokemon =>
-      axios.get(pokemon.url).then(res => (pokemon.details = res))
+    pokemons.map((pokemon) =>
+      axios.get(pokemon.url).then((res) => (pokemon.details = res))
     );
-  }
+  }, []);
 
-  render() {
-    return (
-      <Router>
-        <div className="App">
-          <div className="container">
-            <NavBar />
-            <Route
-              exact
-              path="/pokemons"
-              render={props => (
-                <React.Fragment>
-                  <ul>
-                    <Pokemons getId = {this.getId} getPokemonData={this.getPokemonData} pokemons={this.state.pokemons} />
-                  </ul>
-                </React.Fragment>
-              )}
-            />
-            <Route
-              exact
-              path="/types"
-              render={props => (
-                <React.Fragment>
-                  <Types types={this.state.types} />
-                </React.Fragment>
-              )}
-            />
-            <Route exact path="/pokemon/:id" component={PokemonDetail} />
-            
-            
-          </div>
+  return (
+    <Router>
+      <div className="App">
+        <div className="container">
+          <NavBar />
+          <Route
+            exact
+            path="/pokemons"
+            render={(props) => (
+              <React.Fragment>
+                <ul>
+                  <Pokemons
+                    getId={getId}
+                    getPokemonData={getPokemonData}
+                    pokemons={pokemons}
+                  />
+                </ul>
+              </React.Fragment>
+            )}
+          />
+          <Route
+            exact
+            path="/types"
+            render={(props) => (
+              <React.Fragment>
+                <Types types={types} />
+              </React.Fragment>
+            )}
+          />
+          <Route exact path="/pokemon/:id" component={PokemonDetail} />
         </div>
-      </Router>
-    );
-  }
-}
+      </div>
+    </Router>
+  );
+};
+
 export default App;
